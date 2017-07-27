60182cf6cebcec6931f97889c16a89339489cdcf
forcejosh <jecari08@hotmail.com>
2017-07-27 13:59:03 -0500

    primer commit para el repositorio en github

---
+++ interfaz/src/main/java/com/jeshua/interfaz/InterfazApplication.java
@@ -0,0 +1,12 @@
+package com.jeshua.interfaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfazApplication.class, args);
+	}
+}
